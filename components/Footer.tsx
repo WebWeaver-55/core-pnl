e36267592fece81aeb205ea1aa9ec9b561a2d3ed
Shaurya Sehgal <shauryasehgal555@gmail.com>
2025-09-13 21:00:16 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -149,7 +149,7 @@ export default function Footer() {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                 </svg>
                 <a href="tel:+17873039920" className="hover:text-sky-400 transition-colors">
-                  +1 (787) 303-9920
+                  +1 (782) 303-9920
                 </a>
               </div>
               <div className="flex items-center gap-2">
